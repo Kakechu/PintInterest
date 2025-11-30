c50505cc95839d83db14271b58d8978f1a882b8f
Siniky <ville.makinen03@gmail.com>
2025-11-30 02:46:48 +0200

    photo picking functionality nothing displayed yet

--- app/(tabs)/add-beer.tsx
+++ app/(tabs)/add-beer.tsx
@@ -1,10 +1,26 @@
 import { useTheme } from "@/contexts/ThemeContext";
+import * as ImagePicker from 'expo-image-picker';
 import { Text, View } from "react-native";
 
 import Button from '@/components/ui/button';
 
 export default function AddBeer() {
   const { styles } = useTheme();
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Add a beer.</Text>
@@ -13,7 +29,7 @@ export default function AddBeer() {
       <Text>Description: [input]</Text>
       <Text>This is where you put a photo.</Text>
       <Text>Mark as favorite.</Text>
-      <Button label="Choose a photo" />
+      <Button label="Choose a photo" onPress={pickImageAsync} />
     </View>
   );
 }
