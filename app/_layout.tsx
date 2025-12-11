6e5054da8958957d2db56b05e0c5cf9337ed6100
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-12-11 11:18:09 +0200

    add protected routes (hardcoded)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,33 +6,52 @@ import { BeerProvider } from "@/contexts/BeerContext";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
-const ThemedLayout = () => {
-  // use theme hook here
-  const { isDark } = useTheme();
-  // Add StatusBar below before the Stack
+const isLoggedIn = true;
+const shouldCreateAccount = false;
 
+function ThemedLayout() {
+  const { isDark } = useTheme();
   return (
     <>
       <StatusBar style={isDark ? "light" : "dark"} />
-      <Stack
-        screenOptions={{
-          headerShown: true,
-          headerTitle: () => <Text>PintInterest </Text>,
-          headerRight: () => (
-            <View>
-              <ThemeToggle />
-              <Text>Logout</Text>
-            </View>
-          ),
-        }}
-      />
+      <Stack>
+        {/* Visible only when logged in */}
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: true,
+              headerTitle: () => <Text>PintInterest </Text>,
+              headerRight: () => (
+                <View>
+                  <ThemeToggle />
+                  <Text>Logout</Text>
+                </View>
+              ),
+            }}
+          />
+        </Stack.Protected>
+        {/* Visible only when not logged in and account has been created */}
+        <Stack.Protected guard={!isLoggedIn && !shouldCreateAccount}>
+          <Stack.Screen
+            name={"auth/login"}
+            options={{ title: "Login", headerShown: false }}
+          />
+        </Stack.Protected>
+        {/* Visible only when not logged in and account has not been created */}
+        <Stack.Protected guard={!isLoggedIn && shouldCreateAccount}>
+          <Stack.Screen
+            name={"auth/signup"}
+            options={{ title: "Sign Up", headerShown: false }}
+          />
+        </Stack.Protected>
+      </Stack>
     </>
   );
-};
+}
 
 export default function RootLayout() {
   return (
-    // Wrap the layout with ThemeProvider
     <BeerProvider>
       <ThemeProvider>
         <ThemedLayout />
