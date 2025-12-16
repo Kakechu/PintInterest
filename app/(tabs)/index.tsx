6bd4c8d72c63de9b2432c3b4572fcae36ccc2a11
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-12-16 11:27:30 +0200

    fix front page pressables

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import CustomText from "@/components/ui/custom-text";
 import { useBeers } from "@/contexts/BeerContext";
 import { useTheme } from "@/contexts/ThemeContext";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { Pressable, View } from "react-native";
 
 export default function Index() {
@@ -21,36 +21,30 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <CustomText variant="screenTitle">Welcome to PintInterest</CustomText>
-      <View style={styles.frontPageSectionContainer}>
-        <Ionicons name={"beer"} size={28} color={styles.beerIconColor.color} />
-        <CustomText variant="body">
-          <Link href="/add-beer">Add a beer</Link>
-        </CustomText>
-      </View>
-      <Pressable>
+      <Pressable onPress={() => router.navigate("/add-beer")}>
         <View style={styles.frontPageSectionContainer}>
           <Ionicons
             name={"beer"}
             size={28}
             color={styles.beerIconColor.color}
           />
-          <CustomText variant="body">
-            <Link href="/add-beer">Add a beer</Link>
-          </CustomText>
+          <CustomText variant="body">Add a beer</CustomText>
         </View>
       </Pressable>
-      <View style={styles.frontPageSectionContainer}>
-        <Ionicons name="list" size={28} color={styles.beerIconColor.color} />
-        <CustomText variant="body">
-          <Link href="/my-beers">My beers</Link>
-        </CustomText>
-      </View>
+      <Pressable onPress={() => router.navigate("/my-beers")}>
+        <View style={styles.frontPageSectionContainer}>
+          <Ionicons
+            name={"list"}
+            size={28}
+            color={styles.beerIconColor.color}
+          />
+          <CustomText variant="body">My beers</CustomText>
+        </View>
+      </Pressable>
       <CustomText variant="title">Statistics</CustomText>
-
       <CustomText variant="body">
         <Link href="/my-beers">- You&apos;ve rated {totalBeers} beers</Link>
       </CustomText>
-
       <CustomText variant="body">- Average rating: {averageRating}</CustomText>
       <CustomText variant="body">- You have {favorites} favorites</CustomText>
     </View>
